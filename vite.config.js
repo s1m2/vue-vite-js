f01b85cfd0fcead006cf724cd729767b20f93ecb
Sam Macharia <smm_mwangii@live.com>
2021-07-18 23:35:04 +0100

    Resolving files from the node modules

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import Vue from "@vitejs/plugin-vue";
+import path from 'path';
+import { defineConfig } from "vite";
 
-export default {
+export default defineConfig({
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'node_modules')
+        }
+    },
     plugins: [Vue()]
-}
+})
