89c77596af8cbaaabfcb964510668c8a7ec2dbaf
Sam Macharia <smm_mwangii@live.com>
2021-07-18 22:56:01 +0100

    Initial code push with setup in place

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    This vue project is configured with Vite JS
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
